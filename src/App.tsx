f6f645440e35dc07db9efc675edaa5b6210ed207
Maytê <maytearaujo@yahoo.com.br>
2025-04-07 14:15:00 -0300

    feat: Página de Cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Cadastro from './pages/cadastro/Cadastro';
+<Route path="/" element={<Cadastro />} />
 function App() {
 
   return (
@@ -11,8 +13,9 @@ function App() {
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
+              {/* <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} /> */}
+              <Route path="/" element={<Cadastro />} />
             </Routes>
           </div>
           <Footer />
