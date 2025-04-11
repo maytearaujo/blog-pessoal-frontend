d0c6bdc9f26b29aa8e780a5a684c92c5a006c727
Maytê <maytearaujo@yahoo.com.br>
2025-04-10 22:04:47 -0300

    feat: CRUD Temas

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Cadastro from './pages/cadastro/Cadastro';
 import Login from './pages/login/Login';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormTema from './components/formTema/FormTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
             <Route path="/temas" element={<ListaTemas />} />
             <Route path="/cadastrartema" element={<FormTema />} />
             <Route path="/editartema/:id" element={<FormTema />} />
+            <Route path="/deletartema/:id" element={<DeletarTema />} />
           </Routes>
         </div>
         <Footer />
--- src/components/temas/CardTemas/CardTemas.tsx
+++ src/components/temas/CardTemas/CardTemas.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import Tema from '../../../models/Tema'
 
-interface CardTemasProps{
+interface CardTemasProps {
     tema: Tema
 }
 
@@ -12,16 +12,17 @@ function CardTemas({ tema }: CardTemasProps) {
                 Tema
             </header>
             <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
-            
+
             <div className="flex">
-                <Link to=''
-                    className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 
-                        flex items-center justify-center py-2'>
+                <Link to={`/editartema/${tema.id}`}
+                    className='w-full text-slate-100 bg-	indigo-400 hover:bg-indigo-800 
+    flex items-center justify-center py-2'>
                     <button>Editar</button>
                 </Link>
 
-                <Link to='' className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                    flex items-center justify-center'>
+                <Link to={`/deletartema/${tema.id}`}
+                    className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+		flex items-center justify-center'>
                     <button>Deletar</button>
                 </Link>
             </div>
