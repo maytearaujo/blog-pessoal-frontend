3b1ab4c3d9656536df7671b7ea2a275710500408
Maytê <maytearaujo@yahoo.com.br>
2025-04-11 16:03:07 -0300

    feat: CRUD Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import Login from './pages/login/Login';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
 import FormTema from './components/formTema/FormTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormPostagem from './components/postagens/formPostagem/Formpostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 
 function App() {
 
@@ -26,6 +29,10 @@ function App() {
             <Route path="/cadastrartema" element={<FormTema />} />
             <Route path="/editartema/:id" element={<FormTema />} />
             <Route path="/deletartema/:id" element={<DeletarTema />} />
+            <Route path="/postagens" element={<ListaPostagens />} />
+            <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+            <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+            <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
           </Routes>
         </div>
         <Footer />
